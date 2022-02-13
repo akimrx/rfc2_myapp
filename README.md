93d233f9c6427ede293dd286c470d8980a0b73e5
akimrx <akimstrong@yandex.ru>
2022-02-13 23:09:12 +0300

    init myapp role

---
+++ README.md
@@ -0,0 +1,49 @@
+![Role tests](https://github.com/akimrx/rfc2_nginx/actions/workflows/ci.yml/badge.svg)
+
+Role Name
+=========
+
+Example myapp role.
+
+Role Variables
+--------------
+
+See `defaults/main.yml`
+
+| Variable | Default | Description |
+|----------|---------|-------------|
+| `myaap_port` | `8080` | listening port |
+
+
+
+Running tests locally
+---------------------
+```shell
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+ansible-galaxy collection install community.docker
+
+molecule -v test
+```
+
+Example Playbook
+----------------
+
+Including an example of how to use your role (for instance, with variables passed in as parameters) is always nice for users too:
+
+```yaml
+- hosts: servers
+  roles:
+      - rfc2_myapp
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+akimrx
---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,44 @@
+import pytest
+import requests
+
+
+@pytest.fixture
+def role_vars(host):
+    """Loading standard role variables."""
+    defaults_files = "file=./defaults/main.yml name=role_defaults"
+    vars_files = "file=./vars/main.yml name=role_vars"
+
+    ansible_vars = host.ansible("include_vars", defaults_files)["ansible_facts"]["role_defaults"]
+    ansible_vars.update(
+        host.ansible("include_vars", vars_files)["ansible_facts"]["role_vars"]
+    )
+    return ansible_vars
+
+
+def test_nginx_dependency(host):
+    """Check requirement nginx role."""
+    nginx = host.package("nginx")
+    nginx_service = host.service("nginx")
+    nginx_port = host.socket("tcp://0.0.0.0:80")
+    nginx_port.is_listening, "NGINX port is not listening"
+    assert nginx.is_installed, "NGINX is not installed"
+    assert nginx_service.is_running, "NGINX service is not running"
+    assert nginx_service.is_enabled, "NGINX service is not enabled"
+
+
+def test_application_service(host):
+    """Checking the installation of the MyApp service."""
+    myapp_service = host.service("myapp")
+    assert myapp_service.is_running, "MyApp service is not running"
+    assert myapp_service.is_enabled, "MyApp service is not enabled"
+
+
+def test_application_is_work(host, role_vars):
+    """Check that the application really works."""
+    expected_port = role_vars.get("myapp_port")
+    app_http = host.socket(f"tcp://0.0.0.0:{expected_port}")
+    assert app_http.is_listening, "App HTTP port is not listening"
+
+    http_response = requests.get(f"http://0.0.0.0:{expected_port}")
+    assert http_response.status_code == 200, "HTTP response status is not equal 200"
+    assert http_response.text == "Hello, World!", "Invalid HTTP response"
